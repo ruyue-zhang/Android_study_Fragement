4c22bc6f3c164eb39d0fac01a31945b215c861a0
lpdft <772346730@qq.com>
2020-08-14 15:37:29 +0800

    [ruyue] feat:add two button, them can change color when click

--- app/src/main/java/com/ruyue/fragement/MainActivity.java
+++ app/src/main/java/com/ruyue/fragement/MainActivity.java
@@ -3,6 +3,8 @@ package com.ruyue.fragement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button androidBtn = findViewById(R.id.android);
+        final Button javaBtn = findViewById(R.id.java);
+        androidBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                androidBtn.setBackground(getResources().getDrawable(R.drawable.btn_label_pressed, null));
+                javaBtn.setBackground(getResources().getDrawable(R.drawable.btn_label_up, null));
+            }
+        });
+
+        javaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                javaBtn.setBackground(getResources().getDrawable(R.drawable.btn_label_pressed, null));
+                androidBtn.setBackground(getResources().getDrawable(R.drawable.btn_label_up, null));
+            }
+        });
     }
 }
