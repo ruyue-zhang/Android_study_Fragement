dcba36c53058a236dbfe5afec2563db00c58358e
lpdft <772346730@qq.com>
2020-08-16 14:04:27 +0800

    [ruyue] feat:default display android information when open the app firstly

--- app/src/main/java/com/ruyue/fragement/MainActivity.java
+++ app/src/main/java/com/ruyue/fragement/MainActivity.java
@@ -2,20 +2,28 @@ package com.ruyue.fragement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentAndroid fragmentAndroid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        fragmentAndroid = new FragmentAndroid();
+        getSupportFragmentManager().beginTransaction().add(R.id.my_fl, fragmentAndroid).commitAllowingStateLoss();
+
+
         final Button androidBtn = findViewById(R.id.android);
         final Button javaBtn = findViewById(R.id.java);
         androidBtn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("UseCompatLoadingForDrawables")
             @Override
             public void onClick(View v) {
                 androidBtn.setBackground(getResources().getDrawable(R.drawable.btn_label_pressed, null));
@@ -24,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         javaBtn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("UseCompatLoadingForDrawables")
             @Override
             public void onClick(View v) {
                 javaBtn.setBackground(getResources().getDrawable(R.drawable.btn_label_pressed, null));
